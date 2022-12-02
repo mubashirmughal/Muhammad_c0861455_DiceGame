efa940b67f09cb37af144cfd8090781346e7b1bb
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-12-01 20:04:36 -0500

    added variables and random function for the dice to change by pressing button

--- app/src/main/java/com/example/dicegame1/MainActivity.java
+++ app/src/main/java/com/example/dicegame1/MainActivity.java
@@ -13,14 +13,27 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final Random RANDOM = new Random();
+    private Button rollDices;
+    private ImageView imageView1, imageView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final int dice[] = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+        rollDices = (Button) findViewById(R.id.rollDices);
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
 
+        rollDices.setOnClickListener(v -> {
+            Random random = new Random();
+            int num1 = random.nextInt(6);
+            Random random1 = new Random();
+            int num2 = random.nextInt(6);
+
+            imageView1.setImageResource(dice[num1]);
+            imageView2.setImageResource(dice[num2]);
+        });
     }
-
-
 }
